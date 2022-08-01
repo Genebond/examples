4c18d3dace3b4e44c60aa2dd86ecf3a9094c083f
esteakshapin(mac) <esteakshapin@gmail.com>
2022-08-01 10:32:36 +0600

    finished fetching amount_in quota

---
+++ amount_in_example/src/App.js
@@ -0,0 +1,116 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useRef } from 'react';
+import axios from "axios";
+
+function App() {
+  const [inputAmount, setInputAmount] = useState(null);
+
+  //storing state of the application - fetching information or not
+  const [loading, setLoading] = useState(false);
+
+  const host = "https://qaapius01.fluidefi.io/" //please use your custom URL if you have one
+
+  //url for the gas_price endpoint
+  const url = host + "get_amount_in/";
+
+  //jwt token recieved from authorization endpoint
+  const token = "2a270d2717e9ef7c11c753a9407854525659ca24";
+
+  const inputTokenRef = useRef(null)
+
+  //implement error handling and update UI accordingly
+  const errorHandle = (e) => { console.log(e) };
+
+  //collecting data from input fields and sending it to the api
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+
+    //initial result value
+    let res = null;
+
+    //header configuration for post call
+    const config = {
+      headers: {
+        Authorization: "Token " + token
+      }
+    };
+
+    //payload (post body)
+    const data = {
+      network: e.target.network.value,
+      platform: e.target.platform.value,
+      amount_out: e.target.amount_out.value,
+      path: [e.target.input_token.value, e.target.output_token.value],
+    };
+
+    //make api call to retrieve average gas prices from the latest
+    //block for EIP - 1559 or legacy evm networks
+    try {
+      res = await axios.post(url, data, config);
+
+      console.log(res);
+
+      setInputAmount(res.data.amount_in);
+
+      setLoading(false);
+    } catch (e) {
+      //error handling
+      errorHandle(e);
+      setLoading(false);
+    }
+
+
+
+  }
+  return (
+    <div className="App">
+      <h1>Amount In</h1>
+      <p>Get a quote for a given output amount and swap path from a uniswapv2-compatible router</p>
+      <form onSubmit={handleSubmit}>
+        <label for="network">network: </label>
+        <select id="network" name="network">
+          <option value="bnb">bnb</option>
+          <option value="mainnet">mainnet</option>
+          <option value="ropsten">ropsten</option>
+        </select>
+        <br />
+        <label for="platform">platform: </label>
+        <select id="platform" name="platform">
+          <option value="pancakeswap">pancakeswap</option>
+          <option value="uniswapv2">uniswapv2</option>
+          <option value="sushiswap">sushiswap</option>
+        </select>
+        <br />
+        <label for="input_token">input token: </label>
+        <select id="input_token" name="input_token" ref={inputTokenRef}>
+          <option value="0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d">BNB USDC</option>
+          <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">USDC</option>
+          <option value="0xdAC17F958D2ee523a2206206994597C13D831ec7">USDT</option>
+          <option value="0x4Fabb145d64652a948d72533023f6E7A623C7C53">BUSD</option>
+        </select>
+        <br />
+        <label for="output_token">output token: </label>
+        <select id="output_token" name="output_token">
+          <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">USDC</option>
+          <option value="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c">WBNB</option>
+          <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">USDT</option>
+          <option value="0x4Fabb145d64652a948d72533023f6E7A623C7C53">BUSD</option>
+        </select>
+        <br />
+        <label for="amount_out">amount out: </label>
+        {/* giving a default value for demonstration purposes  */}
+        <input id='amount_out' name='amount_out' placeholder='Enter the desired amount out after swap' defaultValue={100000000000000000000} />
+        <br></br>
+        {loading ? <b>loading</b> : <input type="submit"></input>}
+
+      </form>
+
+      <p>estimated amount required to recieve the desired output amount: <b>{inputAmount}</b> {inputTokenRef.current.options[inputTokenRef.current.selectedIndex].text}</p>
+    </div>
+  );
+}
+
+export default App;
